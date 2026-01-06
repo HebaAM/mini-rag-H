9a532a63273f46e3ff1c7d0197c1e2968c8f52a1
HebaAM <106166228+HebaAM@users.noreply.github.com>
2026-01-06 11:42:57 -0600

    tut-003

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+# So that all routes can access environment variables
+# dotenv loads the variables from a .env file into the system
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+from routes import base
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to the FastAPI app!"}
+app.include_router(base.base_router)
+
